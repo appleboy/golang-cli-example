2a6ee76fb64e3df3f47ae972b77b19f09ca4939a
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-02-16 14:45:11 +0800

    add example 2
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ example02/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+type (
+	// Config information.
+	Config struct {
+		username string
+		password string
+	}
+)
+
+var config Config
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Example"
+	app.Usage = "Example"
+	app.Action = run
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "username,u",
+			Usage:  "user account",
+			EnvVar: "DOCKER_USERNAME",
+		},
+		cli.StringFlag{
+			Name:   "password,p",
+			Usage:  "user password",
+			EnvVar: "DOCKER_PASSWORD",
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func run(c *cli.Context) error {
+	config = Config{
+		username: c.String("username"),
+		password: c.String("password"),
+	}
+
+	return exec()
+}
+
+func exec() error {
+	fmt.Println("username:", config.username)
+	fmt.Println("password:", config.password)
+
+	return nil
+}
